f0c1077a3e1a214e712c4718060232481b1b72b6
Allen Porter <allen@thebends.org>
2022-11-24 16:13:54 -0800

    Add initial skeleton of recurrence rule editor

--- src/recurrence-rule-editor/RecurrenceRuleEditor.ts
+++ src/recurrence-rule-editor/RecurrenceRuleEditor.ts
@@ -1,5 +1,35 @@
 import { html, css, LitElement } from 'lit';
-import { property } from 'lit/decorators.js';
+import { state } from 'lit/decorators.js';
+import '@material/mwc-icon/mwc-icon.js';
+import '@material/mwc-list/mwc-list-item.js';
+import { SelectedDetail } from '@material/mwc-list';
+import { Select } from '@material/mwc-select/mwc-select.js';
+import '@material/mwc-textfield/mwc-textfield.js';
+import {
+  mdiAlphaFCircle,
+  mdiAlphaFCircleOutline,
+  mdiAlphaMCircle,
+  mdiAlphaMCircleOutline,
+  mdiAlphaSCircle,
+  mdiAlphaSCircleOutline,
+  mdiAlphaTCircle,
+  mdiAlphaTCircleOutline,
+  mdiAlphaWCircle,
+  mdiAlphaWCircleOutline,
+} from '@mdi/js';
+import './svg-button-toggle/svg-button-toggle.js';
+
+type RepeatFrequency = 'none' | 'yearly' | 'monthly' | 'weekly' | 'daily';
+
+function intervalSuffix(freq: RepeatFrequency) {
+  if (freq === 'monthly') {
+    return 'months';
+  }
+  if (freq === 'weekly') {
+    return 'weeks';
+  }
+  return 'days';
+}
 
 export class RecurrenceRuleEditor extends LitElement {
   static styles = css`
@@ -10,18 +40,85 @@ export class RecurrenceRuleEditor extends LitElement {
     }
   `;
 
-  @property({ type: String }) title = 'Hey there';
+  @state() private _freq?: RepeatFrequency = 'none';
 
-  @property({ type: Number }) counter = 5;
-
-  __increment(): void {
-    this.counter += 1;
-  }
+  @state() private _interval: number = 1;
 
   render() {
     return html`
-      <h2>${this.title} Mr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <div>
+        <mwc-select
+          id="freq"
+          label="Repeat"
+          @selected=${this._onRepeatSelected}
+        >
+          <mwc-list-item value="none" .selected=${this._freq === 'none'}
+            >None</mwc-list-item
+          >
+          <mwc-list-item value="yearly" .selected=${this._freq === 'yearly'}
+            >Yearly</mwc-list-item
+          >
+          <mwc-list-item value="monthly" .selected=${this._freq === 'monthly'}
+            >Monthly</mwc-list-item
+          >
+          <mwc-list-item value="weekly" .selected=${this._freq === 'weekly'}
+            >Weekly</mwc-list-item
+          >
+          <mwc-list-item value="daily" .selected=${this._freq === 'daily'}
+            >Daily</mwc-list-item
+          >
+        </mwc-select>
+      </div>
+
+      ${this._freq !== 'none' && this._freq !== 'yearly'
+        ? html`
+            <mwc-textfield
+              label="Repeat interval"
+              type="number"
+              min="1"
+              value=${this._interval}
+              suffix=${intervalSuffix(this._freq!)}
+            ></mwc-textfield>
+          `
+        : html``}
+      ${this._freq === 'weekly'
+        ? html`
+            <div>
+              <svg-button-toggle
+                onIcon=${mdiAlphaMCircle}
+                offIcon=${mdiAlphaMCircleOutline}
+              ></svg-button-toggle>
+              <svg-button-toggle
+                onIcon=${mdiAlphaTCircle}
+                offIcon=${mdiAlphaTCircleOutline}
+              ></svg-button-toggle>
+              <svg-button-toggle
+                onIcon=${mdiAlphaWCircle}
+                offIcon=${mdiAlphaWCircleOutline}
+              ></svg-button-toggle>
+              <svg-button-toggle
+                onIcon=${mdiAlphaTCircle}
+                offIcon=${mdiAlphaTCircleOutline}
+              ></svg-button-toggle>
+              <svg-button-toggle
+                onIcon=${mdiAlphaFCircle}
+                offIcon=${mdiAlphaFCircleOutline}
+              ></svg-button-toggle>
+              <svg-button-toggle
+                onIcon=${mdiAlphaSCircle}
+                offIcon=${mdiAlphaSCircleOutline}
+              ></svg-button-toggle>
+              <svg-button-toggle
+                onIcon=${mdiAlphaSCircle}
+                offIcon=${mdiAlphaSCircleOutline}
+              ></svg-button-toggle>
+            </div>
+          `
+        : html``}
     `;
+  }
+
+  _onRepeatSelected(e: CustomEvent<SelectedDetail>) {
+    this._freq = (e.target as Select).value as RepeatFrequency;
   }
 }
