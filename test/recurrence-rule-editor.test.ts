a27e7b57cc66a68fded7944b6461cdcdedbcd7dc
Allen Porter <allen@thebends.org>
2022-11-25 13:43:55 -0800

    Update rrule via day of week toggles

--- test/recurrence-rule-editor.test.ts
+++ test/recurrence-rule-editor.test.ts
@@ -2,6 +2,7 @@ import { html } from 'lit';
 import { fixture, expect, elementUpdated, oneEvent } from '@open-wc/testing';
 import { RecurrenceRuleEditor } from '../src/recurrence-rule-editor/RecurrenceRuleEditor.js';
 import '../src/recurrence-rule-editor/recurrence-rule-editor.js';
+import { ButtonToggle } from '../src/recurrence-rule-editor/button-toggle/button-toggle.js'
 
 describe('RecurrenceRuleEditor', () => {
   it('has a default frequency of "none"', async () => {
@@ -108,6 +109,93 @@ describe('RecurrenceRuleEditor', () => {
     {
       const { detail } = await oneEvent(el, 'value-changed');
       expect(detail.value).to.equal('FREQ=WEEKLY');
+    }
+  });
+
+  it('can set "weekly" recurrence by day of week', async () => {
+    const el = await fixture<RecurrenceRuleEditor>(
+      html`<recurrence-rule-editor></recurrence-rule-editor>`
+    );
+    await elementUpdated(el);
+    
+    const sel = el.shadowRoot!.querySelector('mwc-select')!;
+    setTimeout(async () =>  {
+      sel.select(3);  
+    });
+
+    {
+      const { detail } = await oneEvent(el, 'value-changed');
+      expect(detail.value).to.equal('FREQ=WEEKLY');
+    }
+
+    // eslint-disable-next-line no-undef
+    const toggles: NodeListOf<ButtonToggle> = el.shadowRoot!.querySelectorAll('button-toggle');
+    expect(toggles.length).to.equal(7);
+    
+    setTimeout(async () =>  {
+      toggles[1].shadowRoot!.querySelector('mwc-button')!.click();
+    });
+    {
+      const { detail } = await oneEvent(el, 'value-changed');
+      expect(detail.value).to.equal('FREQ=WEEKLY;BYDAY=MO');
+    }
+  });
+
+
+  it('can multi-select day of week', async () => {
+    const el = await fixture<RecurrenceRuleEditor>(
+      html`<recurrence-rule-editor></recurrence-rule-editor>`
+    );
+    await elementUpdated(el);
+    
+    const sel = el.shadowRoot!.querySelector('mwc-select')!;
+    setTimeout(async () =>  {
+      sel.select(3);  
+    });
+
+    {
+      const { detail } = await oneEvent(el, 'value-changed');
+      expect(detail.value).to.equal('FREQ=WEEKLY');
+    }
+
+    // eslint-disable-next-line no-undef
+    const toggles: NodeListOf<ButtonToggle> = el.shadowRoot!.querySelectorAll('button-toggle');
+    expect(toggles.length).to.equal(7);
+    
+    // Select Monday
+    setTimeout(async () =>  {
+      toggles[1].shadowRoot!.querySelector('mwc-button')!.click();
+    });
+    {
+      const { detail } = await oneEvent(el, 'value-changed');
+      expect(detail.value).to.equal('FREQ=WEEKLY;BYDAY=MO');
+    }
+    
+    // Select Tuesday
+    setTimeout(async () =>  {
+      toggles[2].shadowRoot!.querySelector('mwc-button')!.click();
+    });
+    {
+      const { detail } = await oneEvent(el, 'value-changed');
+      expect(detail.value).to.equal('FREQ=WEEKLY;BYDAY=MO,TU');
+    }
+
+    // De-select Monday
+    setTimeout(async () =>  {
+      toggles[1].shadowRoot!.querySelector('mwc-button')!.click();
+    });
+    {
+      const { detail } = await oneEvent(el, 'value-changed');
+      expect(detail.value).to.equal('FREQ=WEEKLY;BYDAY=TU');
+    }
+
+    // Select Thursday
+    setTimeout(async () =>  {
+      toggles[4].shadowRoot!.querySelector('mwc-button')!.click();
+    });
+    {
+      const { detail } = await oneEvent(el, 'value-changed');
+      expect(detail.value).to.equal('FREQ=WEEKLY;BYDAY=TU,TH');
     }
   });
 });
